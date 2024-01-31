15efc80e78914d35d8f1bcf7bad9c5a11ab19b84
Hassan Shaikh <shaikh.hassan01@outlook.com>
2024-01-31 13:43:13 -0500

    created the function to capture username and pass it as an argument

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,12 +1,27 @@
-var getUserRepos = function (user) {
-    //format the github api url
-    var apiUrl = "https://api.github.com/users/" + user + "/repos";
-    //make a request to the url
-    fetch(apiUrl).then(function(response) {
-        response.json().then(function(data) {
-            console.log(data);
-        })
-    })
+var userFormEl = document.querySelector("#user-form");
+var nameInputEl = document.querySelector("#username");
+
+var formSubmitHandler = function (event) {
+  event.preventDefault();
+  console.log(event);
+  var username = nameInputEl.value.trim();
+  if (username) {
+    getUserRepos(username);
+    nameInputEl.value = "";
+  } else {
+    alert("Please enter a Github username");
+  }
 };
 
-getUserRepos("microsoft");
+var getUserRepos = function (user) {
+  //format the github api url
+  var apiUrl = "https://api.github.com/users/" + user + "/repos";
+  //make a request to the url
+  fetch(apiUrl).then(function (response) {
+    response.json().then(function (data) {
+      console.log(data);
+    });
+  });
+};
+
+userFormEl.addEventListener("submit", formSubmitHandler);
