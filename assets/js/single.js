ec573dd8921200a886d0b44761554766e58b06a5
Hassan Shaikh <shaikh.hassan01@outlook.com>
2024-03-21 17:09:06 -0400

    used data from query parameter to read

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,6 +1,16 @@
 var issueContainerEl = document.querySelector("#issue-container");
 var limitWarningEl = document.querySelector("#limit-warning");
+var repoNameEl = document.querySelector("#repo-name");
 
+//get repo name
+var getRepoName = function() {
+    //get query string
+    var queryString = document.location.search;
+    var repoName = queryString.split("=")[1];
+    getRepoIssues(repoName);
+    repoNameEl.textContent = repoName;
+    
+};
 var getRepoIssues = function (repo) {
   var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
   fetch(apiUrl).then(function (response) {
@@ -69,4 +79,5 @@ var displayWarning = function (repo) {
   //append to warning container
   limitWarningEl.appendChild(linkEl);
 };
-getRepoIssues("facebook/react");
+
+getRepoName();
